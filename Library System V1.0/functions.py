5aea9d84c3448f3e5cc4b1dd829ac1955e49a315
KodedKid <68852080+KodedKid@users.noreply.github.com>
2020-07-31 13:33:46 +0100

    Added V1.0

---
+++ Library System V1.0/functions.py
@@ -0,0 +1,24 @@
+class library():
+    def options():
+        print("-------------------------------------------------------------------------------")
+        print("Please put in the number of the option you would like.")
+        option = input("Would you 1. Like to check out a book or 2. Put a book back or 3. View the log file?")
+        print("You chose: ", option)
+        print("-------------------------------------------------------------------------------")
+        return option
+    def getname():
+        name = input("What is your first and last name? ")
+        return name
+    def getyear():
+        year = input("What year are you in?")
+        return year
+    def initialize():
+        print("Welcome to The Library!")
+        
+        
+        
+
+
+    
+        
+        
